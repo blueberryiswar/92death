95a1068b97bda8fd84f0926f227fa736379d9c30
seveneddy <jessipooopooo@gmail.com>
2019-05-17 22:30:32 +0200

    ui update

--- src/Scenes/UI.js
+++ src/Scenes/UI.js
@@ -17,33 +17,26 @@ export default class UIScene extends Phaser.Scene {
 		this.health = 6;
 	}
 
-
-
-	var coin_still = game.add.sprite(100,100,'coin');
-	sprite.frame = 0;
-
-	setAnimations() {
-		this.scene.anims.create({
-            key: 'coin_still',
-            frames: this.scene.anims.generateFrameNumbers('coin', { start: 0, end: 0 }),
-            frameRate: 4,
-            repeat: -1
-		});
-		this.scene.anims.create({
-            key: 'coin_spinning',
-            frames: this.scene.anims.generateFrameNumbers('coin', { start: 0, end: 5 }),
-            frameRate: 4,
-            repeat: -1
-		});
-
-	}
+//	setAnimations() {
+//		this.scene.anims.create({
+//            key: 'coin_still',
+//            frames: this.scene.anims.generateFrameNumbers('coin', { start: 0, end: 0 }),
+//            frameRate: 4,
+//            repeat: -1
+//		});
+//		this.scene.anims.create({
+//            key: 'coin_spinning',
+//            frames: this.scene.anims.generateFrameNumbers('coin', { start: 0, end: 5 }),
+//  	      frameRate: 4,
+//			  repeat: -1
+//		});
+//	}
 
 	create() {
 		// get Reference to the GameScene
 		this.gameScene = this.scene.get("Game");
 
 		// create Score text
-
 		this.scoreText = this.add.text(12, 12, ` `, {
 			fontSize: "32px",
 			fill: "#fff"
@@ -76,6 +69,6 @@ export default class UIScene extends Phaser.Scene {
 
 	updateDisplay() {
 		this.healthText.setText(`Health: ${this.health}`);
-		this.scoreText.setText(`${this.bucks}`);
+		this.scoreText.setText(`Bucks: ${this.bucks}`);
 	}
 }
